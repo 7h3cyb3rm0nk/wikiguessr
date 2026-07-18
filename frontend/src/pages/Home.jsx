c2c9c4203ad0dfcb0940dd5c9ac88388cbb29f1b
stinsolo <stinasolomon853@gmail.com>
2026-07-18 17:14:38 +0530

    Clean Vite template and organize project structure

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+function Home() {
+  return (
+    <div>
+      <h1>WikiGuessr</h1>
+      <p>Welcome to WikiGuessr!</p>
+
+      <button>Create Room</button>
+      <button>Join Room</button>
+    </div>
+  );
+}
+
+export default Home;
